14b26b8f1a06bedbd790a445e9ba82a8499612b0
Triple <ismatullomuydinov57@gmail.com>
2026-06-02 15:59:09 +0500

    second commit

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,15 @@
 # main/urls.py
 from django.urls import path
-from . views import index, course_students, student_detail
+from . views import index, course_students, student_detail, course_create, student_create, course_update, student_update
 
 urlpatterns = [
     path('', index, name='index'),
     path('course/<int:course_id>/students/', course_students, name='course_students'),
     path('student/<int:student_id>/',student_detail, name='student_detail'),
+    path('course_create/', course_create, name='course_create'),
+    path('course_update/<int:course_id>/', course_update, name='course_update'),
+    path('student_update/<int:student_id>/', student_update, name='student_update'),
+    path('student_create/', student_create, name='student_create'),
+
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 # main/views.py
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Course, Student
+from .forms import CourseForm, StudentForm
 
 def index(request):
     courses = Course.objects.all()
@@ -28,3 +31,62 @@ def student_detail(request, student_id):
         'student': student,
     }
     return render(request, 'main/student_detail.html', context)
+
+
+def course_create(request:HttpRequest):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = CourseForm(data=request.POST)
+            if form.is_valid():
+                course = form.save()
+                return redirect('index')
+        else:
+           form = CourseForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/course_create.html', context)
+
+
+
+def student_create(request:HttpRequest):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = StudentForm(data=request.POST)
+            if form.is_valid():
+                student = form.save()
+                return redirect('student_detail', student_id=student.pk)
+        else:
+           form = StudentForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/student_create.html', context)
+
+def course_update(request:HttpRequest, course_id:int):
+    course=Course.objects.get(pk=course_id)
+    if request.method=='POST':
+        form=CourseForm(data=request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    form=CourseForm(instance=course)
+    context={
+        'course':course,
+        'form':form
+    }
+    return render(request, 'main/course_update.html', context)
+
+def student_update(request:HttpRequest, student_id:int):
+    student=Student.objects.get(pk=student_id)
+    if request.method=='POST':
+        form=StudentForm(data=request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    form=StudentForm(instance=student)
+    context={
+        'student':student,
+        'form':form
+    }
+    return render(request, 'main/student_update.html', context)
